696ec532af7f356f48cb76764d7f8c2f71de0aa7
msi <meibrohimov.@gmail.com>
2024-02-18 14:51:55 +0500

    add new change 🕛

--- buttons.py
+++ buttons.py
@@ -6,8 +6,3 @@ btn = [
 
 ]
 button = types.ReplyKeyboardMarkup(keyboard=btn, resize_keyboard=True)
-
-btn2 = [
-    [types.KeyboardButton(text="salom 1"), types.KeyboardButton(text="salom 2")]
-]
-btn3 = types.ReplyKeyboardMarkup(keyboard=btn2, resize_keyboard=True)
